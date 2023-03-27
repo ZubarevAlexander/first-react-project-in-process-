d2a1fca8ed28a28ad416577439eb66198574f70d
ZubarevAlexander <zybarev95@mail.ru>
2023-03-27 21:50:20 +0300

    Burger + adaptive
    1. Сверстал бургер-меню
    2. Адаптив

--- src/components/Header/HeaderTop/HeaderTop.jsx
+++ src/components/Header/HeaderTop/HeaderTop.jsx
@@ -5,50 +5,59 @@ import telegram from "./../icons/telegram.svg";
 import vk from "./../icons/vk.svg";
 import classmates from "./../icons/classmates.svg";
 
-import "./HeaderTop.css";
+import "./HeaderTop.scss";
+import { useState } from "react";
 
-const HeaderTop = () => {
+const HeaderTop = ({ active, setActive }) => {
+
 	return (
 		<div className="header-top">
 			<div className="header-top__container container">
+				<div className={active ? "header-top__burger _active" : "header-top__burger"} onClick={() => setActive(false)}>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
 				<div className="header-top__menu menu">
-					<ul className="menu__list">
-						<li className="menu__item"><a href="#" className="menu__link">Гарантия свежести</a></li>
-						<li className="menu__item"><a href="#" className="menu__link">Доставка и оплата</a></li>
-						<li className="menu__item"><a href="#" className="menu__link">Оптовые поставки</a></li>
-						<li className="menu__item"><a href="#" className="menu__link">Контакты</a></li>
-					</ul>
-				</div>
-				<div className="header-top__actions actions">
-					<div className="actions__phone">
-						<img src={iconMobile} alt="" />
-						<a href="tel:8 812 309-82-88" >8 812 309-82-88</a>
-					</div>
-					<a href="" className="actions__basket">
-						<div className="basket">
-							<img src={basket} alt="" />
-							<span>0</span>
-						</div>
-						<span >
-							В корзине( <span>0</span> товара )
-						</span>
-					</a>
-					<div className="actions__socials socials">
-						<div className="socials__items">
-							<a href="" className="socials__link">
-								<img src={telegram} alt="" />
+					<div className={active ? "menu__body _active" : "menu__body"} onClick={(e) => e.stopPropagation()}>
+						<ul className="menu__list">
+							<li className="menu__item"><a href="#" className="menu__link">Гарантия свежести</a></li>
+							<li className="menu__item"><a href="#" className="menu__link">Доставка и оплата</a></li>
+							<li className="menu__item"><a href="#" className="menu__link">Оптовые поставки</a></li>
+							<li className="menu__item"><a href="#" className="menu__link">Контакты</a></li>
+						</ul>
+						<div className="header-top__actions actions">
+							<div className="actions__phone">
+								<img src={iconMobile} alt="" />
+								<a href="tel:8 812 309-82-88" >8 812 309-82-88</a>
+							</div>
+							<a href="" className="actions__basket">
+								<div className="basket">
+									<img src={basket} alt="" />
+									<span>0</span>
+								</div>
+								<span >
+									В корзине( <span>0</span> товара )
+								</span>
 							</a>
-							<a href="" className="socials__link">
-								<img src={vk} />
-							</a>
-							<a href="" className="socials__link">
-								<img src={classmates} alt="" />
-							</a>
+							<div className="actions__socials socials">
+								<div className="socials__items">
+									<a href="" className="socials__link">
+										<img src={telegram} alt="telegram" />
+									</a>
+									<a href="" className="socials__link">
+										<img src={vk} alt="vk" />
+									</a>
+									<a href="" className="socials__link">
+										<img src={classmates} alt="classmates" />
+									</a>
+								</div>
+							</div>
 						</div>
 					</div>
 
-
 				</div>
+
 			</div >
 		</div >
 	);
