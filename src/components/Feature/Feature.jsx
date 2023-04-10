c40fe7fb6ae9fcf00c49d5d9a9647290a70d6207
ZubarevAlexander <zybarev95@mail.ru>
2023-04-10 18:10:30 +0300

    Styles for Features
    Стилизация блока Особенностей

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,14 +1,11 @@
 import "./Feature.scss";
+// import bg1 from "./bg/bg1.png";
 
-const Feature = ({ icon, title, text }) => {
+const Feature = ({ icon, title, text, bg }) => {
 	return (
 		<div className="feature__item">
-			<div className="feature__bg">
-				<div className="feature__triangle-up"></div>
-				<div className="feature__triangle-down"></div>
-				{/* <svg  viewBox="0 0 1 1" preserveAspectRatio="none" fill="none"> 
-					<polygon fill="none;" points="0,0 1,0 0,1" />
-				</svg> */}
+			<div className={"feature__bg " + bg}>
+				{/* <img src={bg1}></img> */}
 			</div>
 			<div className="feature__content">
 				<div className="feature__icon">
