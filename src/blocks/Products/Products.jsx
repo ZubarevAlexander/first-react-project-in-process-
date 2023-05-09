ea81fe48eedd921f3d2a0c717212f8cb50a5f2d3
ZubarevAlexander <zybarev95@mail.ru>
2023-05-09 18:04:48 +0300

    Бэк
    Перенес карточки в БЭК
    с помощью mockAPI

--- src/blocks/Products/Products.jsx
+++ src/blocks/Products/Products.jsx
@@ -1,21 +1,32 @@
+import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import TitleH2 from "../../components/TitleH2/TitleH2";
 import "./Products.scss";
 
 
-const Products = ({ products }) => {
+const Products = () => {
+	const [items, setItems] = useState([]);
+
+	useEffect(()=>{
+		fetch("https://645a422e65bd868e9315a23f.mockapi.io/items").then((res) => {
+			return res.json();
+		}).then((json) => {
+			setItems(json);
+		});
+	}, []);
+	
 	return (
 		<>
 			<div className="products__container container">
 				<TitleH2 title="Популярные наборы" />
 				<div className="products__row">
-					
-						{products.map((product) => {
-							return (
-								<ProductCard img={product.img} title={product.title} desc={product.desc} price={product.price} />
-							)
-						})}
-					
+
+					{items.map((product) => {
+						return (
+							<ProductCard img={product.img} title={product.title} desc={product.desc} price={product.price} />
+						)
+					})}
+
 				</div>
 				<div className="products__btn">Все праздничные наборы</div>
 			</div>
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import "./ProductCard.scss";
 
 
-const ProductCard = ({ img, title, desc, price }) => {
+const ProductCard = (props) => {
+	
 
 	const [isAdded, setIsAdded] = useState(false);
 
@@ -14,21 +15,20 @@ const ProductCard = ({ img, title, desc, price }) => {
 		<div className="products__column">
 			<div className="products__item">
 				<div className="products__image">
-					<img src={img}></img>
+					<img src={props.img} alt="Картинка"></img>
 				</div>
 				<div className="products__content">
-					<h3 className="products__title">{title}</h3>
-					<div className="products__desc">{desc}</div>
+					<h3 className="products__title">{props.title}</h3>
+					<div className="products__desc">{props.desc}</div>
 				</div>
 				<div className="products__price">
-					<span>{price}</span>
+					<span>{props.price} руб.</span>
 					<button onClick={addToBasket} className={isAdded ? "products__add checked" : "products__add"}>
 						<div className={isAdded ? "products__basket checked" : "products__basket"}>
 							<span className={isAdded ? "checked" : ""}>В корзину</span>
 						</div>
 					</button>
 				</div>
-
 			</div>
 		</div>
 	);
